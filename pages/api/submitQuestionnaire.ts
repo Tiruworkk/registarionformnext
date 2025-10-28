66a011d6bbfc8e133b8c943403451fb9e3c917c1
Tiruworkk <tiruworkekassa@gmail.com>
2025-10-28 11:38:07 +0300

    Fix formatting in submitQuestionnaire.ts

--- pages/api/submitQuestionnaire.ts
+++ pages/api/submitQuestionnaire.ts
@@ -14,7 +14,7 @@ try {
 interface Data {
   message: string;
 }
-
+ 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   if (req.method !== 'POST') return res.status(405).json({ message: 'Method not allowed' });
 
