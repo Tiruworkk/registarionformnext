a39214804856a9210ae3f5d7aaa6472a39407de8
Tiruworkk <tiruworkekassa@gmail.com>
2025-10-28 17:40:34 +0300

    Update register.ts

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import clientPromise from "../../lib/mongodb";
 
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
